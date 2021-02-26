5ab9273a5791e6ee77ebe426d894d0330643280f
badhon1512 <badhon1512@gmail.com>
2021-02-27 01:56:04 +0600

    productlist module has created

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,7 +1,25 @@
 
 import Header from './Header';
+import {useEffect,useState} from 'react'
+import {Table} from 'react-bootstrap'
 
 const ProductList=()=>{
+
+  const [productlist,setProductList]=useState([])
+
+
+  useEffect(async()=>{
+
+    let product=await fetch('http://localhost:8000/api/productlist')
+    product=await product.json()
+    setProductList(product)
+    
+
+  },[])
+
+  console.log(productlist)
+
+  
 
 
   return(
@@ -11,17 +29,33 @@ const ProductList=()=>{
      
      <h1>Product List</h1>
 
-     <table>
-
-      <th>
-        <td>Id</td>
-        <td>Name</td>
-        <td>Image</td>
-        <td>description</td>
-        <td>Price</td>
-      </th>
-     </table>
-
+     <Table striped bordered hover>
+  <thead>
+    <tr >
+      <th>id</th>
+      <th>name</th>
+      <th>picture</th>
+      <th>description</th>
+      <th>price</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+      productlist.map((product)=>{
+       return(
+      <tr key={product.id}>
+      <td>{product.id}</td>
+      <td>{product.name}</td>
+      <td><img src="https://static.toiimg.com/photo/70072353.cms" style={{width:"100px"}}/></td>
+      <td>{product.description}</td>
+      <td>{product.price}</td>
+      
+    </tr>
+       )
+      })
+    }
+  </tbody>
+</Table>
 
     </div>
   )
